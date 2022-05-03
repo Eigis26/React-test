f92a62d0a1c41924f94ca9868b985aa59df96242
unknown <eigirdas.user@gmail.com>
2022-05-03 09:48:50 +0300

    Upload project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [greet, setGreet] = useState("");
+  const [name, setName] = useState(false);
+  const clickas = () => {
+  const inp = document.getElementById("inp").value;
+    setName (inp);
+    setGreet (true);
+    if (greet){
+      setGreet(false);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input id="inp" type = "text" ></input>
+      <button onClick = {clickas}>{!greet ? "show": "hide"}</button> 
+      <br />
+      <h1>{greet ? `Hello ${name}`: "" }</h1>
     </div>
   );
 }
